29916779eb4cbeeec47881336425e77c93d2b06f
engadnanhossain <engr.adnanhossain@gmail.com>
2023-11-01 23:34:35 +0600

    add some pages with route

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layout/Main'
 import Home from '../pages/home/Home'
 import About from '../pages/about/About'
+import Shop from '../pages/shop/Shop'
+import Blog from '../pages/blog/Blog'
+import MyAccount from '../pages/account/MyAccount'
+import Contact from '../pages/contact/Contact'
 
 export const router = createBrowserRouter([
     {
@@ -15,6 +19,26 @@ export const router = createBrowserRouter([
             {
                 path: '/about',
                 element: <About />
+            },
+            {
+                path: '/shop',
+                element: <Shop />
+            },
+            {
+                path: '/blog',
+                element: <Blog />
+            },
+            {
+                path: '/about',
+                element: <About />
+            },
+            {
+                path: '/my-account',
+                element: <MyAccount />
+            },
+            {
+                path: '/contact',
+                element: <Contact />
             }
         ]
     }
