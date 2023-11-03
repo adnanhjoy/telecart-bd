a822ecabc11bed381a6edb60b3b571c5fb0900bd
engadnanhossain <engr.adnanhossain@gmail.com>
2023-11-03 11:44:08 +0600

    add banner image

--- src/components/home/outlet/BannerSlider.tsx
+++ src/components/home/outlet/BannerSlider.tsx
@@ -4,21 +4,13 @@ import banner1 from '../../../assets/banner/banner1.jpg';
 import banner2 from '../../../assets/banner/banner2.jpg';
 import banner3 from '../../../assets/banner/banner3.jpg';
 
-const contentStyle: React.CSSProperties = {
-    height: '300px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-};
-
 const BannerSlider: React.FC = () => {
     return (
         <div>
             <Carousel autoplay>
-                <img style={contentStyle} src={banner1} alt="" />
-                <img style={contentStyle} src={banner2} alt="" />
-                <img style={contentStyle} src={banner3} alt="" />
+                <img className='h-72' src={banner1} alt="" />
+                <img className='h-72' src={banner2} alt="" />
+                <img className='h-72' src={banner3} alt="" />
             </Carousel>
         </div>
     );
