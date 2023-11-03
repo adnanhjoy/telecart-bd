34368bd944ce3ac3a47a00ee17dac402591cfc52
engadnanhossain <engr.adnanhossain@gmail.com>
2023-11-03 09:32:57 +0600

    add carousel

--- src/components/home/outlet/BannerSlider.tsx
+++ src/components/home/outlet/BannerSlider.tsx
@@ -1,9 +1,31 @@
+import { Carousel } from 'antd';
 import React from 'react';
+
+const contentStyle: React.CSSProperties = {
+    height: '300px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 
 const BannerSlider: React.FC = () => {
     return (
         <div>
-            <h1>Banner</h1>
+            <Carousel autoplay>
+                <div>
+                    <h3 style={contentStyle}>1</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>2</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>3</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>4</h3>
+                </div>
+            </Carousel>
         </div>
     );
 };
