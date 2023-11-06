719c17740d410173bdd8007c11b299bf43fca7b1
engadnanhossain <engr.adnanhossain@gmail.com>
2023-11-06 23:37:31 +0600

    change arrow key

--- src/components/home/sidebar/SubCategory.tsx
+++ src/components/home/sidebar/SubCategory.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+// import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 const SubCategory: React.FC = () => {
@@ -7,7 +7,7 @@ const SubCategory: React.FC = () => {
         <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded'>
             <span className='flex items-center justify-between'>
                 <Link to='/' className='block text-gray-500'>Exercise & Fitness</Link>
-                <MdOutlineKeyboardArrowRight />
+                {/* <MdOutlineKeyboardArrowRight /> */}
             </span>
         </li>
     );
