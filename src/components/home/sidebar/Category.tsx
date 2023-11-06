208cc9221f8739da795f29fe64f5a1fe62e2686a
engadnanhossain <engr.adnanhossain@gmail.com>
2023-11-06 23:30:42 +0600

    add subcategory and applu hover effect

--- src/components/home/sidebar/Category.tsx
+++ src/components/home/sidebar/Category.tsx
@@ -1,36 +1,112 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import SubCategory from './SubCategory';
 
 const Category: React.FC = () => {
     return (
         <div>
             <h1 className='bg-primary p-2 text-white font-semibold text-center rounded'>Category</h1>
-            <ul>
-                <li className='p-2 flex items-center justify-between cursor-pointer hover:bg-yellow-50 w-full rounded'>
-                    <Link to='/' className='block text-gray-500'>Electron Product</Link>
-                    <MdOutlineKeyboardArrowRight />
+            <ul className='relative'>
+                <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded group'>
+                    <span className='flex items-center justify-between'>
+                        <Link to='/' className='block text-gray-500'>Electron Product</Link>
+                        <MdOutlineKeyboardArrowRight />
+                    </span>
+                    <ul className='absolute w-full left-full bg-white z-50 top-0 hidden group-hover:block'>
+                        {
+                            Array(7).fill(null).map((_, index) =>
+                                <SubCategory
+                                    key={index}
+                                />)
+                        }
+                    </ul>
                 </li>
-                <li className='p-2 flex items-center justify-between cursor-pointer hover:bg-yellow-50 w-full rounded'>
-                    <Link to='/' className='block text-gray-500'>Mens Fashion</Link>
-                    <MdOutlineKeyboardArrowRight />
+                <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded group'>
+                    <span className='flex items-center justify-between'>
+                        <Link to='/' className='block text-gray-500'>Mens & Fashion</Link>
+                        <MdOutlineKeyboardArrowRight />
+                    </span>
+                    <ul className='absolute w-full left-full bg-white z-50 top-0 hidden group-hover:block'>
+                        {
+                            Array(7).fill(null).map((_, index) =>
+                                <SubCategory
+                                    key={index}
+                                />)
+                        }
+                    </ul>
                 </li>
-                <li className='p-2 flex items-center justify-between cursor-pointer hover:bg-yellow-50 w-full rounded'>
-                    <Link to='/' className='block text-gray-500'>Sport Products</Link>
-                    <MdOutlineKeyboardArrowRight />
+                <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded group'>
+                    <span className='flex items-center justify-between'>
+                        <Link to='/' className='block text-gray-500'>Womens & Fashion</Link>
+                        <MdOutlineKeyboardArrowRight />
+                    </span>
+                    <ul className='absolute w-full left-full bg-white z-50 top-0 hidden group-hover:block'>
+                        {
+                            Array(7).fill(null).map((_, index) =>
+                                <SubCategory
+                                    key={index}
+                                />)
+                        }
+                    </ul>
                 </li>
-                <li className='p-2 flex items-center justify-between cursor-pointer hover:bg-yellow-50 w-full rounded'>
-                    <Link to='/' className='block text-gray-500'>Womens Fashion</Link>
-                    <MdOutlineKeyboardArrowRight />
+                <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded group'>
+                    <span className='flex items-center justify-between'>
+                        <Link to='/' className='block text-gray-500'>Beauty & Health</Link>
+                        <MdOutlineKeyboardArrowRight />
+                    </span>
+                    <ul className='absolute w-full left-full bg-white z-50 top-0 hidden group-hover:block'>
+                        {
+                            Array(7).fill(null).map((_, index) =>
+                                <SubCategory
+                                    key={index}
+                                />)
+                        }
+                    </ul>
                 </li>
-                <li className='p-2 flex items-center justify-between cursor-pointer hover:bg-yellow-50 w-full rounded'>
-                    <Link to='/' className='block text-gray-500'>Home & Lifestyle</Link>
-                    <MdOutlineKeyboardArrowRight />
+                <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded group'>
+                    <span className='flex items-center justify-between'>
+                        <Link to='/' className='block text-gray-500'>Mothers & Baby</Link>
+                        <MdOutlineKeyboardArrowRight />
+                    </span>
+                    <ul className='absolute w-full left-full bg-white z-50 top-0 hidden group-hover:block'>
+                        {
+                            Array(7).fill(null).map((_, index) =>
+                                <SubCategory
+                                    key={index}
+                                />)
+                        }
+                    </ul>
                 </li>
-                <li className='p-2 flex items-center justify-between cursor-pointer hover:bg-yellow-50 w-full rounded'>
-                    <Link to='/' className='block text-gray-500'>Groceries</Link>
-                    <MdOutlineKeyboardArrowRight />
+                <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded group'>
+                    <span className='flex items-center justify-between'>
+                        <Link to='/' className='block text-gray-500'>Home & Lifestyle</Link>
+                        <MdOutlineKeyboardArrowRight />
+                    </span>
+                    <ul className='absolute w-full left-full bg-white z-50 top-0 hidden group-hover:block'>
+                        {
+                            Array(7).fill(null).map((_, index) =>
+                                <SubCategory
+                                    key={index}
+                                />)
+                        }
+                    </ul>
                 </li>
+                <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded group'>
+                    <span className='flex items-center justify-between'>
+                        <Link to='/' className='block text-gray-500'>Support & Outdoors</Link>
+                        <MdOutlineKeyboardArrowRight />
+                    </span>
+                    <ul className='absolute w-full left-full bg-white z-50 top-0 hidden group-hover:block'>
+                        {
+                            Array(7).fill(null).map((_, index) =>
+                                <SubCategory
+                                    key={index}
+                                />)
+                        }
+                    </ul>
+                </li>
+
             </ul>
         </div>
     );
---
+++ src/components/home/sidebar/SubCategory.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
+const SubCategory: React.FC = () => {
+    return (
+        <li className='p-2 cursor-pointer hover:bg-yellow-50 w-full rounded'>
+            <span className='flex items-center justify-between'>
+                <Link to='/' className='block text-gray-500'>Electron Product</Link>
+                <MdOutlineKeyboardArrowRight />
+            </span>
+        </li>
+    );
+};
+
+export default SubCategory;
