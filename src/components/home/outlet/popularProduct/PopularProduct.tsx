696e28116f90abe9bebb06f9df433f6c29ca1f51
engadnanhossain <engr.adnanhossain@gmail.com>
2023-11-08 19:53:34 +0600

    add animation in react slick

--- src/components/home/outlet/popularProduct/PopularProduct.tsx
+++ src/components/home/outlet/popularProduct/PopularProduct.tsx
@@ -6,15 +6,17 @@ import ProductCard from '../productSection/ProductCard';
 
 const PopularProduct: React.FC = () => {
     var settings = {
-        dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 5
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        cssEase: "linear"
     };
     return (
         <div className='my-4'>
-            <h5 className='font-semibold text-primary'>Products for you</h5>
+            <h5 className='font-semibold text-primary mb-4'>Popular Products</h5>
             <Slider {...settings}>
                 {
                     Array(20).fill(null).map((_, index) =>
--- src/components/home/outlet/productSection/ProductSection.tsx
+++ src/components/home/outlet/productSection/ProductSection.tsx
@@ -5,7 +5,7 @@ const ProductSection: React.FC = () => {
     return (
         <div className='mt-4'>
             <h5 className='font-semibold text-primary'>Products for you</h5>
-            <div className='grid grid-cols-2 md:grid-cols-5 gap-2 mt-2'>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-2 mt-2'>
                 {
                     Array(20).fill(null).map((_, index) =>
                         <ProductCard
