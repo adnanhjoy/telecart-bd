dd9eb06bcadc2a1f5b61db378436aa715534f13f
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-29 22:03:56 +0600

    add profile page content

--- src/pages/account/Profile.tsx
+++ src/pages/account/Profile.tsx
@@ -4,6 +4,25 @@ const ProfilePage: React.FC = () => {
     return (
         <div className='border-t border-t-primary pt-4'>
             <h1 className='text-2xl font-medium text-primary'>My Accoutn Details</h1>
+
+            <form action="">
+                <div className='mt-4'>
+                    <label htmlFor="" className='font-medium'>Name:</label>
+                    <input className='border rounded p-2 w-1/2 outline-none block border-dark mt-2' type="text" />
+                </div>
+
+                <div className='mt-4'>
+                    <label htmlFor="" className='font-medium'>Email:</label>
+                    <input className='border rounded p-2 w-1/2 outline-none block border-dark mt-2' type="email" />
+                </div>
+
+                <div className='mt-4'>
+                    <label htmlFor="" className='font-medium'>Phone:</label>
+                    <input className='border rounded p-2 w-1/2 outline-none block border-dark mt-2' type="text" />
+                </div>
+
+                <button className='bg-primary px-6 py-2 rounded-md text-white hover:opacity-90 mt-4'>Save</button>
+            </form>
         </div>
     );
 };
