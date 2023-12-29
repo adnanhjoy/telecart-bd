146c6598e8c099af5b43bf1919a08d9489870270
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-29 09:54:28 +0600

    update horizontal product card

--- src/components/card/HorizontalProductCard.tsx
+++ src/components/card/HorizontalProductCard.tsx
@@ -7,7 +7,7 @@ const HorizontalProductCard: React.FC = () => {
             <div className='flex items-start cursor-pointer gap-2 bg-white rounded'>
                 <img className='w-1/5' src="https://www.shopz.com.bd/wp-content/uploads/2021/12/LIGE-9866-Men-Quartz-Watch-1.jpg" alt="" />
                 <div>
-                    <h3 className='text-xs line-clamp-2 hover:underline text-dark'>Indispensable -Quality And Durablity -OLEVS Leather Waterproof Quartz Fashion Watch for Men- Innovative Remarkable - Disclose Styles & Luxe</h3>
+                    <h3 className='text-sm leading-5 line-clamp-2 hover:underline text-dark'>Indispensable -Quality And Durablity -OLEVS Leather Waterproof Quartz Fashion Watch for Men- Innovative Remarkable - Disclose Styles & Luxe</h3>
                     <div className='flex items-center justify-between mt-2 mr-2'>
                         <span className='flex items-center gap-1'>
                             <Rate className='text-sm gap-0' allowHalf defaultValue={4.5} disabled />
